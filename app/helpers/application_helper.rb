92625302acf1f974ef98a05ab322e5b510f26339
Winston Coronell <coronell@gmail.com>
2014-04-03 15:53:18 -0600

    minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis
 	def full_title(page_title)
-		base_title = "Shortcut"
+		base_title = "Coronellw"
 		if page_title.empty?
 			base_title
 		else
